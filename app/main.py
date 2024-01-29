72cf3c070818a8a24a2c49a633d65509718af088
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2024-01-29 15:49:23 +0400

    5.3 Понимание процессов миграции баз данных Начало работы с Alembic Рабочий процесс Alembic Управление версиями и ревизии Откаты

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.chapter_3.router import router as router_3
 from app.chapter_4.router import router as router_4
 from app.chapter_5.router import router as router_5
-
+from app.chapter_6.router import router as router_6
 
 app = FastAPI(
     title="FastAPI на Stepik"
@@ -12,4 +12,4 @@ app = FastAPI(
 app.include_router(router_3)
 app.include_router(router_4)
 app.include_router(router_5)
-
+app.include_router(router_6)
